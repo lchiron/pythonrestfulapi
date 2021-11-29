20bb7ae0cb5b83b6227956d3c5be64d5468996cf
edward.li <edward.li@farfetch.com>
2021-11-29 16:21:45 +0800

    comepte

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 # from flask_jwt import JWT
 from flask_jwt_extended import JWTManager
 # from security import authenticate, identity
 from resources.user import UserRegister, User, UserLogin
-from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from resources.item import Item, ItemList
 from db import db
+from blacklist import BLACKLIST
 
 
 app = Flask(__name__)
@@ -17,6 +18,8 @@ app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLACHEMY_TRACK_MODIFICATIONS'] = False
 # show error details
 app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 app.secret_key = "Edward"
 api = Api(app)
 
@@ -40,6 +43,33 @@ def add_claims_to_jwt(identity):
         return {'is_admin': True}
     return {'is_admin': False}
 
+
+@jwt.expired_token_loader
+def expired_token_callback():
+    return jsonify({
+        'description': 'The token has expired.',
+        'error': 'token_expired'
+    }), 401
+    
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return jsonify({
+        'description': 'Signature verification failed.',
+        'error': 'invalid_token'
+    }), 401
+    
+# @jwt.unauthorized_loader
+
+# @jwt.needs_fresh_token_loader
+
+# @jwt.revoked_token_loader 
+ 
+
+@jwt.token_in_blocklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    return decrypted_token['identity'] in BLACKLIST
+
+    
 # customize JWT auth response, include user_id in response body
 # @jwt.auth_response_handler
 # def customized_response_handler(access_token, identity):
--- resources/user.py
+++ resources/user.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required
 from models.user import UserModel
 
 
